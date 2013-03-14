299f97c2e0625972b6240d82c2f79a2b5b78df28
Ronan Chauvin <Ronan@MacBook-Pro-de-Ronan.local>
2013-03-14 13:35:38 -0400

    Add a patch for node_handle.h.

--- build.sh
+++ build.sh
@@ -7,22 +7,27 @@ SRCDIR=`pwd`
 #===============================================================================
 #BOOST
 
-git clone git://gitorious.org/~galbraithjoseph/boostoniphone/galbraithjosephs-boostoniphone.git boostonios
+[ -d $SRCDIR/boostonios ] && rm -rf $SRCDIR/boostonios
+git clone git://gitorious.org/~galbraithjoseph/boostoniphone/galbraithjosephs-boostoniphone.git $SRCDIR/boostonios
 
-(cd $SRCDIR/boostonios; boost.sh);
+cd $SRCDIR/boostonios
+sh boost.sh
 
 #===============================================================================
 #LOG4CXX
 
-(cd $SRCDIR/log4cxx; build.sh);
+cd $SRCDIR/log4cxx
+sh build.sh
 
 #===============================================================================
 #ROS
 
+[ -d $SRCDIR/ros/frameworks ] && rm -rf $SRCDIR/ros/frameworks
 mkdir $SRCDIR/ros/frameworks
 mv $SRCDIR/boostonios/ios/framework/boost.framework $SRCDIR/ros/frameworks/
 mv $SRCDIR/log4cxx/log4cxx.framework $SRCDIR/ros/frameworks/
 
-(cd $SRCDIR/ros; build.sh);
+cd $SRCDIR/ros
+sh build.sh
 
 #===============================================================================
